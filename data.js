cc73935f196fe38d74c1bb2b48989082cf9e9c9f
meoponly <meoponly123@gmail.com>
2025-12-16 23:37:03 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,152 @@
+const chapterData = [
+  {
+    "id": "CH-01",
+    "name": "Module (Only PDF)",
+    "folderId": "1BrM4b4qPjtMiFCM-AtcrZ4qSOJdWPOaB"
+  },
+  {
+    "id": "CH-02",
+    "name": "Practice Sheet || (Only PDF)",
+    "folderId": "1tt8PA2ENh5zi84WhxsvDadSgjxHTt8jY"
+  },
+  {
+    "id": "CH-03",
+    "name": "Resources",
+    "folderId": "1A9gd_ti-WGP_f95mHQ5gXGG9wQEzm6hz"
+  },
+  {
+    "id": "CH-04",
+    "name": "IOQM Important Questions",
+    "folderId": "1MeXbC0mu4wQWUmHlwClqFe8jr6kLCJrR",
+    "notesId": "13Rs-NoIfKMSIM4sMzfYtuN2zOnlw1KuW"
+  },
+  {
+    "id": "CH-05",
+    "name": "Basic Mathematics",
+    "folderId": "1rVV17F-FZGkGem8kz5PpiPSi26rQF9dd",
+    "notesId": "1006uFz68zBmA8HfPhlHD39VpkK0_zdSq",
+    "dppId": "1oSR8tjuJWFWreL6lvRUj4c1VNYP8FQ--"
+  },
+  {
+    "id": "CH-06",
+    "name": "Number Theory (Part-01)",
+    "folderId": "1X5uk2MjZkvbOQdv4g-71TYXjD0r_uxZG",
+    "notesId": "1DTafPoBdLFdrPBHWFYvya_GG257pFN4Q",
+    "dppId": "1E88UoEyJsRZ3j2merENct_Fbyv4ypjnJ"
+  },
+  {
+    "id": "CH-07",
+    "name": "Number Theory (Part-02)",
+    "folderId": "1u9rpisiSL6UHjvRFtQPiIRAxQ-sE_bqE",
+    "notesId": "1SsnAWbf7F612aHaj4Ltg_5E6vxPF4Rg6",
+    "dppId": "1f-2ZrLnx6mvHYL1x-YZJCnWMWrMEjTer"
+  },
+  {
+    "id": "CH-08",
+    "name": "Number Theory (Part-03)",
+    "folderId": "1irmmaXj7dblJViGGZrfWef3TYMB2kfhj",
+    "notesId": "1X-Oz2G9PPhZdday0k_bqJAit25x7YFpH",
+    "dppId": "1Qr85DbUc5vTh5eLchvuS0ZpdCPP_djSc"
+  },
+  {
+    "id": "CH-09",
+    "name": "Algebra (Polynomial and Quadratic Equation)",
+    "folderId": "14xpcYieOP4fapAKuRl5rQdEVLrjI1dnk",
+    "notesId": "1SSQwc58_6YwmVDGJ6ZdK7acmNEPwlXHo",
+    "dppId": "1tpHA6Dljei7CuPL_MFg3D3g3kbExzWyl"
+  },
+  {
+    "id": "CH-10",
+    "name": "Algebra (Sequence and Series)",
+    "folderId": "1ge6mzH5Yfok_U8iAjrcgTPPdXhVfoR5L",
+    "notesId": "1MLsfkxlezNU63T-iSXjbLdWIe3ykIepx",
+    "dppId": "1ZsmkBKjEjw1GySexPGS9ulH0M-iS4Hd2"
+  },
+  {
+    "id": "CH-11",
+    "name": "Recurrence Relation",
+    "folderId": "13WvCBWJLPNVDEoQRmDty0hm33VQ_QkG_",
+    "notesId": "1SDNVGyJP_7KqWsaS_urMk5YPKJZxZJrJ",
+    "dppId": "1vQE6NAJcAXzVX-GnjovhZ1j7uvqw5Uv9"
+  },
+  {
+    "id": "CH-12",
+    "name": "Inequalities",
+    "folderId": "1rp4mVlO2Kl_xG8_YutHAdSkGIZSVXXTu",
+    "notesId": "1N0QEoktUpgJbrXWpORkPpL8EDzDUUy0Y",
+    "dppId": "1XbZNy14inFK3cddKdsnVs5QHf0Y5TGbZ"
+  },
+  {
+    "id": "CH-13",
+    "name": "Geometry (Triangles)",
+    "folderId": "1kngaycRuJS3cO3NT7jSK6_S4ewsShy8P",
+    "notesId": "1MCkKsfkyiUI3BeAoA5YojsAMC0SWXURl",
+    "dppId": "11kzeLCuI4DM0quSaDQuMf7Da-TM4fEeY"
+  },
+  {
+    "id": "CH-14",
+    "name": "Geometry (Quadrilaterals and Circles)",
+    "folderId": "1y5j2NT7EvA_FZPMnUL1_dSttTS_UkCKf",
+    "notesId": "1DX3QrE-oZG0gvaYn2B-_wGHR_nDhFjpp",
+    "dppId": "10_4ZvwuS7AsRM_CoUTEjRwnz7uiEAWOA"
+  },
+  {
+    "id": "CH-15",
+    "name": "Geometry (Coordinate Geometry)",
+    "folderId": "1UaLk6guj3m0_XNelGPFoYCZuzDClehmy",
+    "notesId": "1wCMuAkwyELM2maVlOmgsTLiZOww5iKCF",
+    "dppId": "1LN7sFvEJiC6oadWsdVy3lGDrLE4vcFoW"
+  },
+  {
+    "id": "CH-16",
+    "name": "Binomial Theorem",
+    "folderId": "1FJwd_qOnR2W5z5nnB-qSbGewfAUX6nBJ",
+    "notesId": "1ZLhfK3Vam2N900PH62Cg1gcT7eICLYfu",
+    "dppId": "1EL6Ot6qkOGwwZ6NZFQDqXXDVptaruhq_"
+  },
+  {
+    "id": "CH-17",
+    "name": "Combinatorics (Part-01)",
+    "folderId": "13H_eBE5eO4w03c44ZB5vEqWxnL5-RXhG",
+    "notesId": "1e1W8LrsfR2_AYtgvjSLd24GEiOBuzNDF",
+    "dppId": "1ePNZAzUF-S2ftSKYX9JKWUne1dgl3Ovf"
+  },
+  {
+    "id": "CH-18",
+    "name": "Combinatorics (Part-02)",
+    "folderId": "1_sa5nxSj2NVPJbIZMw_g62QjnyOmxc5o",
+    "notesId": "1pxpkRhKipxmVv7YHz3nDRGyyetKa1czV",
+    "dppId": "1tEuZ1BkUwhZzfvbKKKsSkmN7QQ8ks3kb"
+  },
+  {
+    "id": "CH-19",
+    "name": "Probability",
+    "folderId": "18JE9I5_Cc6NBQrso_L_aeQ5Lo-3lF4sU",
+    "notesId": "16Jxo2A6_PQNKM5X041KXNQ9xJGIHAcgd",
+    "dppId": "1r1IocMqa-4Yu7satf_PMODyolh4Mo9b2"
+  },
+  {
+    "id": "CH-20",
+    "name": "Algebra (Part-05)",
+    "folderId": "16fRTTe2XTVYSsFUyVrucEvgDLNw-oxF4",
+    "notesId": "14bZG-Hqw90QetsbtGRzRlCbcQ3day1Cb"
+  },
+  {
+    "id": "CH-21",
+    "name": "Charcha Session",
+    "folderId": "1TVY87iL6CGdFzVF8Ze5YP52cldP_vQk9",
+    "notesId": "15f0luEioTvctPUOXoI18My0pVR5b7joO"
+  },
+  {
+    "id": "CH-22",
+    "name": "Doubt Session",
+    "folderId": "1KR6o3aKyzG9eRlyQOb1776vLrg1Y5xKX",
+    "notesId": "1QD17NscShXvElzFA7sglGqpEmOLM4wtY"
+  },
+  {
+    "id": "CH-23",
+    "name": "Extra Session",
+    "folderId": "14C4YG-DBiFxO5wwpP0lBiOD03TFsCwqP",
+    "notesId": "1NVwh-JtsVHk0SJhwQv_rPArV_ee0NLE9"
+  }
+];
